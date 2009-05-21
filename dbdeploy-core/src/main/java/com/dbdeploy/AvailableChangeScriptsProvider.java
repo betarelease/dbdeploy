bb384419154de4896c318285efc4d017c17b440d
betarelease <sudhindra.r.rao@gmail.com>
2009-05-21 14:14:31 -0500

    Sudhindra: removed maven structure

--- dbdeploy-core/src/main/java/com/dbdeploy/AvailableChangeScriptsProvider.java
+++
@@ -1,9 +0,0 @@
-package com.dbdeploy;
-
-import com.dbdeploy.scripts.ChangeScript;
-
-import java.util.List;
-
-public interface AvailableChangeScriptsProvider {
-	List<ChangeScript> getAvailableChangeScripts();
-}
